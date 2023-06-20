2ec886a984d8dffe4a0935e26bd3a77d63005a8f
vishal <poke.fcr@gmail.com>
2023-06-20 23:03:59 +0530

    added remote paths for production

--- projects/shell/src/main.ts
+++ projects/shell/src/main.ts
@@ -1,5 +1,9 @@
 import { loadManifest } from '@angular-architects/module-federation';
-loadManifest('assets/mf.manifest.json')
+import { environment } from './environments/environment'
+
+loadManifest(environment.production ? 'assets/mf.manifest.prod.json' : 'assets/mf.manifest.json' )
 .catch((err) => console.error('Error loading remote entries', err))
 .then(() => import('./bootstrap'))
 .catch((err) => console.error(err));
+
+console.log(environment.production)
